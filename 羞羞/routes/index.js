4c3a925ae20af1ef108cfcf749be5d416346b25d
Jlin <565774991@qq.com>
2018-02-17 00:11:01 +0800

    微调

--- 羞羞/routes/index.js
+++ 羞羞/routes/index.js
@@ -21,35 +21,44 @@ router.get('/:num', function (req, res, next) {
     .set('Connection', 'keep-alive')
     .end(function (err, data) {
       if (err) { console.log(err) }
-      console.log('hehe' + JSON.stringify(data))
+      // console.log('hehe' + JSON.stringify(data))
       var $ = cheerio.load(data.text)
       var dirName = $('.main-title').text()
       var maxNum = $('.pagenavi a').eq(4).text()
-      console.log('文件名' + dirName)
-      console.log('最大数量' + maxNum)
+      // console.log('文件名' + dirName)
+      // console.log('最大数量' + maxNum)
       maxNum = Number(maxNum)
       for (var i = 1; i <= maxNum; i++) {
         childNums.push(i)
       }
       console.log(childNums)
-      mkdirp(dir + '/' + dirName, function (err) {
+      mkdirp(dir + '/' + num, function (err) {
         if (err) {
           console.log(err);
         }
-        else console.log(dir + '文件夹创建成功!');
-        async.mapLimit(childNums, 20, function (childNum, cb) {
+        console.log(dir + '文件夹创建成功!');
+        async.mapLimit(childNums, 2, function (childNum, cb) {
           superagent.get('http://www.mzitu.com/' + num + '/' + childNum)
-            .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36')
+            .set('User-Agent', 'Chrome/39.0.2171.99')
             .set('Connection', 'keep-alive')
+            .set('Referer', 'http://www.mzitu.com/' + num)
             .end(function (err, data) {
               var $ = cheerio.load(data.text)
               var url = $('.main-image img').attr('src')
               console.log('图片链接' + url)
               var filename = childNum + '.jpg'
-              request({ uri: url, encoding: 'binary' }, function (error, response, body) {
+              var options = {
+                 url: url,
+                encoding: 'binary',
+                 headers: {
+                 'User-Agent': 'request',
+                'Referer':'http://www.mzitu.com/' + num
+                }
+                };
+              request(options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                   if (!body) console.log("(╥╯^╰╥)哎呀没有内容。。。")
-                  fs.writeFile(dir + '/' + dirName + '/' + filename, body, 'binary', function (err) {
+                  fs.writeFile(dir + '/' + num + '/' + filename, body, 'binary', function (err) {
                     if (err) { console.log(err); }
                     console.log('o(*￣▽￣*)o偷偷下载' + dir + '/' + filename + ' done');
                     cb(null, null)
