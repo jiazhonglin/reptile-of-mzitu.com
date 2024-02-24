1c5491e9722cfbed834bdd462c20e4924a1e110a
jlinjia <jlinjia@tencent.com>
2024-02-24 21:46:40 +0800

    d

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('等静态文件打包好了再部署上来');
 });
 
 module.exports = router;
